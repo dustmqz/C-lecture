df049d7dc27905a63aa969f90aedbeb4b6c64ce2
dustmqz <61788748+dustmqz@users.noreply.github.com>
2020-03-31 23:06:40 +0900

    Create 4_8.c

---
+++ 4_8.c
@@ -0,0 +1,7 @@
+// 4.8 변환 지정자의 수식어들
+// Modifiers
+// 출력 옵션을 다양하게 바꿀 수 있음.
+// %[flags][width][.precision][length]specifier
+// ex) printf("%+10.5hi", 256); // +: 왼쪽으로 붙여서 출력해 // 10:총 열자리를 마련해라 
+// .5:정밀도를 나타내는 숫자는 5개 // h: short
+
