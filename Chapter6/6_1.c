b3e05c475f591b972c358129f02ca28f31ce4db3
dustmqz <61788748+dustmqz@users.noreply.github.com>
2020-06-11 23:47:35 +0900

    Update 6_1.c

--- Chapter6/6_1.c
+++ Chapter6/6_1.c
@@ -2,12 +2,23 @@
 
 //먼저 예제 하나 숫자 계속 입력받고 q입력받으면 지금까지 입력한 숫자들 더한 값 출력하는 프로그램
 
+/*
+Enter an integer (q to quit) : 1
+Enter next integer (q to quit) : 2
+Enter next integer (q to quit) : 5
+Enter next integer (q to quit) : 10
+Enter next integer (q to quit) : 3
+Enter next integer (q to quit) : q
+Sum = 21
+*/
+
+
 #include <stdio.h>
 
 int main()
 {
   int num, sum = 0;
-  scanf(num, &d);
+  scanf(%d, &num);
   while num != 'q';
     printf("Enter an integer (q to quit) : ", num);
     sum += num;
