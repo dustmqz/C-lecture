c5cabee2a8e9d1e35665276b2196794b8ca3369d
dustmqz <61788748+dustmqz@users.noreply.github.com>
2020-06-11 23:39:56 +0900

    Create 6_1.c

---
+++ Chapter6/6_1.c
@@ -0,0 +1,18 @@
+// 6.1 while 반복 루프에서 scanf() 의 반환값 사용하기
+
+//먼저 예제 하나 숫자 계속 입력받고 q입력받으면 지금까지 입력한 숫자들 더한 값 출력하는 프로그램
+
+#include <stdio.h>
+
+int main()
+{
+  int num, sum = 0;
+  scanf(num, &d);
+  while num != 'q';
+    printf("Enter an integer (q to quit) : ", num);
+    sum += num;
+  printf("Sum = ", sum);
+}
+  
+  
+  
