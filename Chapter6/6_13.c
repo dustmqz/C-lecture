6618bbc835b5511ebdc924a1a8188c37d0ea720d
dustmqz <61788748+dustmqz@users.noreply.github.com>
2020-06-27 23:02:31 +0900

    6.13 탈출조건 루프 do while

--- Chapter6/6_13.c
+++ Chapter6/6_13.c
@@ -5,4 +5,32 @@
 
 int main()
 {
-  const int
+  const int i = 337;
+  int a = 0;
+  
+  while (i != a){
+    printf("Enter secret code : \n");
+    scanf("%d", &a);
+  }
+  
+  printf("Good!\n");
+
+
+  // 품
+
+  do  // 일단해라!
+  {
+    printf("Enter secret code : \n");
+    scanf("%d", &a);
+  }
+  while(i != a); // 이럴때 까지!
+
+  printf("Good!");
+
+  return 0;
+}
+
+
+// while 문 보다는 for문을 많이쓰고 do while문 보다는 while 문을 더 많이쓴다. 
+
+
